c224fdde3a2a153b6da4c5a19e4e94ca1ed5ff60
Nickson Kipruto Rop <nickyrutto@gmail.com>
2024-03-29 12:39:38 +0300

    add new routes and set a simple authentication extension

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,7 +1,8 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
 app = Flask(__name__)
-
+auth = HTTPBasicAuth()
 tasks = [
     {
         'id': 1,
@@ -17,16 +18,29 @@ tasks = [
     }
 ]
 
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task':task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
@@ -39,7 +53,7 @@ def create_task():
         'done': False
     }
     tasks.append(task)
-    return jsonify({'task': task}), 201
+    return jsonify({'task': make_public_task(task)}), 201
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
@@ -57,7 +71,7 @@ def update_task(task_id):
     task[0]['title'] = request.json.get('title', task[0]['title'])
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task':task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
@@ -71,5 +85,14 @@ def delete_task(task_id):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'])
+        else:
+            new_task[field] = task[field]
+    return new_task
 if __name__ == '__main__':
     app.run(debug=True)
